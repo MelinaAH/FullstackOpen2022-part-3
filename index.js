e55cec8a38bc92a6a8fc7b03660347b810ffdacd
Melina Aalto-Halme <melina.aalto-halme@tuni.fi>
2023-01-24 14:47:11 +0200

    3.4 backend step4 done

--- index.js
+++ index.js
@@ -24,6 +24,10 @@ let persons = [
     },
 ]
 
+app.get('/api/persons', (request, response) => {
+    response.json(persons);
+  })
+
 app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id);
     const person = persons.find(person => person.id === id);
@@ -35,6 +39,13 @@ app.get('/api/persons/:id', (request, response) => {
     else {
         response.status(404).end();
     }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id);
+    persons = persons.filter(person => person.id != id);
+
+    response.status(204).end();
 })
 
 const PORT = 3001;
