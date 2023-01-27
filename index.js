9830887b76c3f560b354cfd67be3858880caabe6
Melina Aalto-Halme <melina.aalto-halme@tuni.fi>
2023-01-27 20:10:21 +0200

    3.9 backend step9 done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
@@ -29,6 +30,7 @@ let persons = [
 app.use(express.json());
 app.use(morgan('tiny'));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+app.use(cors());
 
 function generateId(maxNumber) {
     return Math.floor(Math.random() * maxNumber);
