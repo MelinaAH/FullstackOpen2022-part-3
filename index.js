3fa1c782a20bbdf94a43a2eb7845b49edee4897a
Melina Aalto-Halme <melina.aalto-halme@tuni.fi>
2023-01-30 09:53:51 +0200

    3.10 backend step 10 index.js file updated

--- index.js
+++ index.js
@@ -96,6 +96,7 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end();
 })
 
-const PORT = 3001;
-app.listen(PORT);
-console.log(`Server is running on port ${PORT}`);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
